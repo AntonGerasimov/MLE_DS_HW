2235e2d4a3ae7ace22852f78f2064a1f383b5320
Anton_Gerasimov <antonsergeevichgerasimov@gmail.com>
2024-01-23 22:28:07 +0400

    Adding utility files

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from torch import nn
 
 def singleton(class_):
     instances = {}
@@ -17,3 +18,25 @@ def configure_logging() -> None:
     """Configures logging"""
     logging.basicConfig(level=logging.INFO, 
                         format='%(asctime)s - %(levelname)s - %(message)s')
+    
+class IrisPredictionModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer_1 = nn.Linear(4, 64)
+        self.layer_2 = nn.Linear(64, 64)
+        self.layer_out = nn.Linear(64, 3)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.1)
+        self.batchnorm1 = nn.BatchNorm1d(64)
+        self.batchnorm2 = nn.BatchNorm1d(64)
+
+    def forward(self, inputs):
+        x = self.relu(self.layer_1(inputs))
+        x = self.batchnorm1(x)
+        x = self.relu(self.layer_2(x))
+        x = self.batchnorm2(x)
+        x = self.dropout(x)
+        x = self.layer_out(x)
+
+        return x
